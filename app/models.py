9e0b6fcf8271a7a227e556d5bde142982b4a58f6
gichimux <gichimumwai1@gmail.com>
2019-05-01 23:45:58 +0300

    created blog post class

--- app/models.py
+++ app/models.py
@@ -1,12 +1,11 @@
-import datetime
-from flask_login import UserMixin, AnonymousUserMixin
-from flask import current_app, request, url_for
-from hashlib import md5
-from werkzeug.security import generate_password_hash, check_password_hash
-from markdown import markdown
 import bleach
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-
+from flask import current_app, request, url_for
+from hashlib import md5
+import datetime
+from flask_login import UserMixin, AnonymousUserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from markdown import markdown
 from .errors import ValidationError
 from . import db, lm, whooshee
 
@@ -24,3 +23,37 @@ class User(UserMixin,db.Model):
     last_seen = db.Column(db.DateTime)
     followed = db.relationship('Follow',foreign_keys = [Follow.follower_id], backref = db.backref('follower', lazy='joined'),lazy = 'dynamic',cascade = 'all, delete-orphan')
     followers = db.relationship('Follow',foreign_keys=[Follow.followed_id],backref=db.backref('followed', lazy='joined'),lazy='dynamic',cascade='all, delete-orphan')
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['ADMINMAIL']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
+    def operation(self, permissions):
+        return self.role is not None and \
+               (self.role.permissions & permissions) == permissions
+
+    def is_administrator(self):
+        return self.operation(Permission.ADMINISTER)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            follower = Follow(follower=self, followed=user)
+            db.session.add(follower)
+
+class Blog_post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(64))
+    disabled = db.Column(db.Boolean)
+    body = db.Column(db.Text)
+    view_num = db.Column(db.Integer, default=0)
+    body_html = db.Column(db.Text)
+    draft = db.Column(db.Boolean, default=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    like_num = db.relationship('Like', backref='blog_post', lazy='dynamic')
+    comments = db.relationship('Comment', backref='blog_post', lazy='dynamic')
