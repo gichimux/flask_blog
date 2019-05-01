e12a303f208c1d236c264619306430d66cb86cfa
gichimux <gichimumwai1@gmail.com>
2019-05-01 23:50:24 +0300

    created role class

--- app/models.py
+++ app/models.py
@@ -89,3 +89,12 @@ class Like(db.Model):
 
     liker_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+
+    users = db.relationship('User', backref = 'role', lazy='dynamic')
